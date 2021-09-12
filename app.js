5ee59a0b3239035f30129e625a20721cd0a97b03
Linda <soldi.linda@gmail.com>
2021-09-12 18:36:59 +0200

    adjusted the bug. finished the project.

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll(".nav-link.custom-link");
 
 setInterval(() => {
-
     for (let i = 0; i < sections.length; i++) {
-        for (let j = 0; j < navLinks.length; j++) {
-
-
-            if (inViewport(sections[i]) && ("#" + sections[i].id == navLinks[j].getAttribute("href"))) {
-                console.log("#" + sections[i].id);
-                console.log(navLinks[j].getAttribute("href"));
-                console.log(("#" + sections[i].id == navLinks[j].getAttribute("href")));
-                navLinks[j].className += " active";
+        if (inViewport(sections[i])) {
+            for (let j = 0; j < navLinks.length; j++) {
+                //for each section checks if the link takes to that section
+                if ("#" + sections[i].id == navLinks[j].getAttribute("href")) {
+                    if (navLinks[j].className.includes("active")) {
+                        //if already active, do nothing
+                        break;
+                    }
+                    //otherwise, make that active
+                    navLinks[j].className += " active";
+                }
+                else {
+                    // if they don't correspond, it means we're not where the link lands
+                    navLinks[j].className = navLinks[j].className.replace("active", "");
+                }
             }
-            else {
-                navLinks[j].className.replace("active", "");
-            }
+
         }
-    }
-
-    function inViewport(el) {
-
-        let r, html;
-
-        if (!el || 1 !== el.nodeType) {
-            return false;
-        }
-
-        html = document.documentElement;
-        r = el.getBoundingClientRect();
-
-        return (r.bottom >= 0
-            && r.right >= 0
-            && r.top <= html.clientHeight
-            && r.left <= html.clientWidth
-        );
     }
 }, 1500);
+
+// boolean, returns true if the element is in the viewport
+function inViewport(el) {
+
+    let r, html;
+
+    if (!el || 1 !== el.nodeType) {
+        return false;
+    }
+
+    html = document.documentElement;
+    r = el.getBoundingClientRect();
+
+    return (r.bottom >= 0
+        && r.right >= 0
+        && r.top <= html.clientHeight
+        && r.left <= html.clientWidth
+    );
+}
